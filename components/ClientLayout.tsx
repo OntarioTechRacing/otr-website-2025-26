1338fa959e8c320a0e7e1fae393974b612f072ae
Saif Siddiqui <saifullah.siddiqui04@gmail.com>
2025-11-24 15:01:35 -0500

    fixed join us layout

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -1,23 +1,4 @@
-'use client';
 
-        
-import { useTheme } from './ThemeProvider';
-import Header from './Header';
-
-interface ClientLayoutProps {
-  children: React.ReactNode;
-}
-
-export default function ClientLayout({ children }: ClientLayoutProps) {
-  const { theme } = useTheme();
-  
-  return (
-    <>
-      <Header theme={theme} />
-      {children}
-    </>
-  );
-}
 'use client';
 
 import { useTheme } from './ThemeProvider';
